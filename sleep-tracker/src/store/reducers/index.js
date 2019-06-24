0a402a70b88bf6be1da5ef71f018c4f9a53d7a86
alexisjcarr <acarr59@gmail.com>
2019-06-24 16:48:37 -0500

    Added requisite register and login reducer cases

--- sleep-tracker/src/store/reducers/index.js
+++ sleep-tracker/src/store/reducers/index.js
@@ -13,6 +13,44 @@ const initialState = {
 
 export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    /* REGISTER USER */
+    case actionType.REGISTER_PENDING:
+      return {
+        ...state,
+        registering: true,
+        error: ""
+      };
+    case actionType.REGISTER_SUCCESS:
+      return {
+        ...state,
+        registering: false,
+        error: ""
+      };
+    case actionType.REGISTER_FAILURE:
+      return {
+        ...state,
+        registering: false,
+        error: action.payload
+      };
+    /* LOGIN USER */
+    case actionType.LOGIN_PENDING:
+      return {
+        ...state,
+        loggingIn: true,
+        error: ""
+      };
+    case actionType.LOGIN_SUCCESS:
+      return {
+        ...state,
+        loggingIn: false,
+        error: ""
+      };
+    case actionType.LOGIN_FAILURE:
+      return {
+        ...state,
+        logginIn: false,
+        error: action.payload
+      };
     default:
       return state;
   }
