d5da77b4dc6422969107629d78f2282d6a64b832
alexisjcarr <acarr59@gmail.com>
2019-06-24 13:11:19 -0500

    Created CreateUserPage component

---
+++ sleep-tracker/src/Components/CreateUserPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import CreateUserForm from "./CreateUserForm";
+import Header from "./Header";
+
+const CreateUserPage = props => {
+  return (
+    <div>
+      <Header />
+      <CreateUserForm />
+    </div>
+  );
+};
+
+export default CreateUserPage;
