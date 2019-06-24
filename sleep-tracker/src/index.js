f5721b340a4ab3ac6bccf18677c5c7db7db6623c
alexisjcarr <acarr59@gmail.com>
2019-06-24 12:45:37 -0500

    Added rootReducer

--- sleep-tracker/src/index.js
+++ sleep-tracker/src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
 
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 import { indigo, blue } from "@material-ui/core/colors";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const theme = createMuiTheme({
   palette: {
